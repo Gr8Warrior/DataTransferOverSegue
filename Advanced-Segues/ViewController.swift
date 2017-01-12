627588e56cfcf9f6e880cc0193512ae04e975b04
shailendrasuriyal <shailendra.suriyal@intimetec.com>
2017-01-12 14:23:45 +0530

    Data Transfer Over segue Done

--- Advanced-Segues/ViewController.swift
+++ Advanced-Segues/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var userName = "Shailu"
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+
+    var activeRow:Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        activeRow = indexPath.row
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4;
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondVC" {
+            
+            let secondVC = segue.destination as! SecondViewController
+            secondVC.activeRow = activeRow ;
+            
+        }
+    }
 
 }
 
